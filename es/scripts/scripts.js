9cf32299ea1cf589e568497b2a94c4d896b6ed2a
mocxd <mark.adkins.hastings@gmail.com>
2018-09-10 14:18:16 -0400

    Update 2018-09-10T18:18:15.013Z

--- es/scripts/scripts.js
+++ es/scripts/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+    var pos1 = $('#pos1').offset().top;
+    var pos2 = $('#pos2').offset().top - 100;
+
+    var showhidebtn = function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll > pos1 && scroll < pos2) {
+            $('#sign-button').removeClass('floating-button--hide');
+        } else {
+            $('#sign-button').addClass('floating-button--hide');
+        }
+    };
+
+    // $(window).scroll($.debounce(100, showhidebtn));
+    $(window).scroll(showhidebtn);
+
+
     // Smooth Scrolling Function
     $('a[href*=#]:not([href=#])').click(function () {
         var $targ = $(this.hash),
