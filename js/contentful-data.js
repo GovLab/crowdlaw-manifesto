28e6a4d2c7ff9f3dc4df2926b5bfcbcf95e00897
mocxd <mark.adkins.hastings@gmail.com>
2018-09-10 14:18:05 -0400

    formatting and layout

--- js/contentful-data.js
+++ js/contentful-data.js
@@ -7,51 +7,68 @@ var container = document.getElementById('content');
 var containerOrgs = document.getElementById('content-orgs');
 
 contentfulClient.getEntries({
-    content_type: 'signature',
-    'fields.signingForOrganization[match]' : 'Signing as Individual',
-    order: 'fields.fullName'
-  })
-  .then(function(entries) {
-    container.innerHTML = render(entries.items)
-  })
+  content_type: 'signature',
+  'fields.signingForOrganization[match]' : 'Signing as Individual',
+  order: 'fields.fullName'
+})
+.then(function(entries) {
+  container.innerHTML = render(entries.items)
+})
 
 contentfulClient.getEntries({
-    content_type: 'signature',
-    'fields.signingForOrganization[match]' : 'Signing on Behalf of Organization',
-    order: 'fields.organization'
-  })
-  .then(function(entries) {
-    containerOrgs.innerHTML = renderOrgs(entries.items)
-  })
+  content_type: 'signature',
+  'fields.signingForOrganization[match]' : 'Signing on Behalf of Organization',
+  order: 'fields.organization'
+})
+.then(function(entries) {
+  containerOrgs.innerHTML = renderOrgs(entries.items)
+})
 
 function render(items) {
+  var split = Math.ceil(items.length / 3);
+  var col1 = items.splice(0, split);
+  var col2 = items.splice(0, split);
   return '<div class="signatures">' +
-    items.map(renderSingle).join('\n') +
-    '</div>'
+  '<div class="row"><div class="column small-12 medium-12 large-4">' +
+  col1.map(renderSingle).join('\n') +
+  '</div><div class="column small-12 medium-12 large-4">' +
+  col2.map(renderSingle).join('\n') +
+  '</div><div class="column small-12 medium-12 large-4">' +
+  items.map(renderSingle).join('\n') +
+  '</div></div></div>'
 }
 
 function renderSingle(items) {
   var fields = items.fields
   return '<div class="signatures__signature">' +
-    fields.fullName +
-    '</div>'
+  fields.fullName + '<div class="signatures__signature__org">' +
+  ((typeof fields.title === 'undefined') ? '' : fields.title + ', ') +
+  ((typeof fields.organization === 'undefined') ? '' : (fields.organization)) +
+  '</div></div>'
 }
 
 function renderOrgs(items) {
   var orgnames = [];
+  items.map(function (i) {
+    if (!orgnames.includes(i.fields.organization)) {
+      orgnames.push(i.fields.organization);
+    }
+  });
+  var split = Math.ceil(orgnames.length / 3);
+  var col1 = orgnames.splice(0, split);
+  var col2 = orgnames.splice(0, split);
   return '<div class="signatures">' +
-    items.map(function (i) {
-      if (!orgnames.includes(i.fields.organization)) {
-        orgnames.push(i.fields.organization);
-        return renderSingleOrg(i);
-      }
-    }).join('\n') +
-    '</div>'
+  '<div class="row"><div class="column small-12 medium-12 large-4">' +
+  col1.map(renderSingleOrg).join('\n') +
+  '</div><div class="column small-12 medium-12 large-4">' +
+  col2.map(renderSingleOrg).join('\n') +
+  '</div><div class="column small-12 medium-12 large-4">' +
+  orgnames.map(renderSingleOrg).join('\n') +
+  '</div></div></div>'
 }
 
 function renderSingleOrg(items) {
-  var fields = items.fields
   return '<div class="signatures__signature">' +
-    fields.organization +
-    '</div>'
+  items +
+  '</div>'
 }
