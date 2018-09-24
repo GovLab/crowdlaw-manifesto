57b95795967b8798ee84d729691519bccb41f8e9
mocxd <mark.adkins.hastings@gmail.com>
2018-09-24 12:08:20 -0400

    Update 2018-09-24T16:08:18.637Z

--- es/js/contentful-data.js
+++ es/js/contentful-data.js
@@ -6,29 +6,66 @@ var contentfulClient = contentful.createClient({
 var container = document.getElementById('content');
 var containerOrgs = document.getElementById('content-orgs');
 
-contentfulClient.getEntries({
-  content_type: 'signature',
-  'fields.signingForOrganization[match]' : 'Signing as Individual',
-  order: 'fields.fullName'
-})
-.then(function(entries) {
-  container.innerHTML = render(entries.items)
-})
+var multiRequest = function(_client, _options, callback, _skip, allEntries) {
+    if (typeof _skip === 'undefined') { _skip = 0; }
+    if (typeof allEntries === 'undefined') { allEntries = []; }
+    _options.skip = _skip;
+    _client.getEntries(_options)
+    .then(function(entries) {
+        console.log('Getting entries', _skip, '-', _skip+1000, 'of', entries.total);
+        if (entries.items.length > 0) {
+            allEntries = allEntries.concat(entries.items);
+        } else {
+            throw 'No entries found';
+        }
+        _skip += 1000;
+        if (_skip < entries.total) {
+            multiRequest(_skip, allEntries);
+        } else {
+            console.log('done');
+            callback(allEntries);
+        }
+    })
+    .catch(console.error)
+}
 
-contentfulClient.getEntries({
-  content_type: 'signature',
+multiRequest(contentfulClient,
+  { content_type: 'signature',
+  'fields.signingForOrganization[match]' : 'Signing as Individual',
+  limit: 1000,
+  order: 'fields.fullName' } , render);
+
+multiRequest(contentfulClient,
+  { content_type: 'signature',
   'fields.signingForOrganization[match]' : 'Signing on Behalf of Organization',
-  order: 'fields.organization'
-})
-.then(function(entries) {
-  containerOrgs.innerHTML = renderOrgs(entries.items)
-})
+  limit: 1000,
+  order: 'fields.organization' } , renderOrgs);
+
+// contentfulClient.getEntries({
+//   content_type: 'signature',
+//   'fields.signingForOrganization[match]' : 'Signing as Individual',
+//   limit: 1000,
+//   order: 'fields.fullName'
+// })
+// .then(function(entries) {
+//   container.innerHTML = render(entries.items)
+// })
+
+// contentfulClient.getEntries({
+//   content_type: 'signature',
+//   'fields.signingForOrganization[match]' : 'Signing on Behalf of Organization',
+//   limit: 1000,
+//   order: 'fields.organization'
+// })
+// .then(function(entries) {
+//   containerOrgs.innerHTML = renderOrgs(entries.items)
+// })
 
 function render(items) {
   var split = Math.ceil(items.length / 3);
   var col1 = items.splice(0, split);
   var col2 = items.splice(0, split);
-  return '<div class="signatures">' +
+  container.innerHTML = '<div class="signatures">' +
   '<div class="row"><div class="column small-12 medium-12 large-4">' +
   col1.map(renderSingle).join('\n') +
   '</div><div class="column small-12 medium-12 large-4">' +
@@ -39,12 +76,12 @@ function render(items) {
 }
 
 function renderSingle(items) {
-  var fields = items.fields
+  var fields = items.fields;
   return '<div class="signatures__signature">' +
   fields.fullName + '<div class="signatures__signature__org">' +
   ((typeof fields.title === 'undefined') ? '' : fields.title + ', ') +
   ((typeof fields.organization === 'undefined') ? '' : (fields.organization)) +
-  '</div></div>'
+  '</div></div>';
 }
 
 function renderOrgs(items) {
@@ -57,7 +94,7 @@ function renderOrgs(items) {
   var split = Math.ceil(orgnames.length / 3);
   var col1 = orgnames.splice(0, split);
   var col2 = orgnames.splice(0, split);
-  return '<div class="signatures">' +
+  containerOrgs.innerHTML = '<div class="signatures">' +
   '<div class="row"><div class="column small-12 medium-12 large-4">' +
   col1.map(renderSingleOrg).join('\n') +
   '</div><div class="column small-12 medium-12 large-4">' +
@@ -70,5 +107,5 @@ function renderOrgs(items) {
 function renderSingleOrg(items) {
   return '<div class="signatures__signature">' +
   items +
-  '</div>'
+  '</div>';
 }
--- es/scripts/scripts.js
+++ es/scripts/scripts.js
@@ -122,191 +122,191 @@ $(document).ready(function () {
         $(latestTimelineItem).addClass("timeline-boxes__item--latest")
     });
 
-    // List.js Implementation
-    var fuzzyOptions = {
-      searchClass: "fuzzy-search",
-      location: 0,
-      distance: 100,
-      threshold: 0.0,
-      multiSearch: true
-    };
-    var options = {
-    valueNames: [ {name:'name', attr:'data-target'}, {name:'country', attr:'data-target'}, {name:'region', attr:'data-target'}, {name:'level', attr:'data-target'}, {name:'organization', attr:'data-target'}, {name:'engagement', attr:'data-target'}, {name:'prog', attr:'data-target'} ],
-        plugins: [ ListFuzzySearch(fuzzyOptions) ]
-    };
+    // // List.js Implementation
+    // var fuzzyOptions = {
+    //   searchClass: "fuzzy-search",
+    //   location: 0,
+    //   distance: 100,
+    //   threshold: 0.0,
+    //   multiSearch: true
+    // };
+    // var options = {
+    // valueNames: [ {name:'name', attr:'data-target'}, {name:'country', attr:'data-target'}, {name:'region', attr:'data-target'}, {name:'level', attr:'data-target'}, {name:'organization', attr:'data-target'}, {name:'engagement', attr:'data-target'}, {name:'prog', attr:'data-target'} ],
+    //     plugins: [ ListFuzzySearch(fuzzyOptions) ]
+    // };
 
-    var companyList = new List('company_data', options);
-    companyList.sort('name', { order: "asc" });
+    // var companyList = new List('company_data', options);
+    // companyList.sort('name', { order: "asc" });
 
-    function searchReset() {
-        $(".fuzzy-search").val("");
-        clearTextSearch();
-        companyList.search();
-    }
+    // function searchReset() {
+    //     $(".fuzzy-search").val("");
+    //     clearTextSearch();
+    //     companyList.search();
+    // }
 
-    // Filter by name
-    $(".fuzzy-search").keyup(function() {
-        var searchString = $(this).val();
-        companyList.fuzzySearch.search(searchString, ["name"]);
-    });
+    // // Filter by name
+    // $(".fuzzy-search").keyup(function() {
+    //     var searchString = $(this).val();
+    //     companyList.fuzzySearch.search(searchString, ["name"]);
+    // });
 
-    $(".js-open-table-search").on("click", function(e) {
-       $($(this).attr('data-target')).focus();
-    })
+    // $(".js-open-table-search").on("click", function(e) {
+    //    $($(this).attr('data-target')).focus();
+    // })
 
-    // Xs and ESC TO CLOSE OUT FORM
-    var searchButtons = $('.table-sortable__search').find("button[type='submit']")
+    // // Xs and ESC TO CLOSE OUT FORM
+    // var searchButtons = $('.table-sortable__search').find("button[type='submit']")
 
-    searchButtons.on("click", function(e) {
-        e.preventDefault();
-        if ($(this).parent().hasClass("table-sortable__search--active")) {
-            $(this).parent().removeClass("table-sortable__search--active");
-            searchReset();
-        }
-    });
+    // searchButtons.on("click", function(e) {
+    //     e.preventDefault();
+    //     if ($(this).parent().hasClass("table-sortable__search--active")) {
+    //         $(this).parent().removeClass("table-sortable__search--active");
+    //         searchReset();
+    //     }
+    // });
 
-    function clearTextSearch() {
-        $('.table-sortable__search--active').each(function(){
-            $(this).removeClass('table-sortable__search--active');
-        });
-    }
+    // function clearTextSearch() {
+    //     $('.table-sortable__search--active').each(function(){
+    //         $(this).removeClass('table-sortable__search--active');
+    //     });
+    // }
 
-    $("body").keyup(function(event) {
-        if ( event.keyCode == "27" ) {
-            $(this).parent().find('.table-sortable__search').removeClass("table-sortable__search--active");
-            searchReset();
-        }
-    });
+    // $("body").keyup(function(event) {
+    //     if ( event.keyCode == "27" ) {
+    //         $(this).parent().find('.table-sortable__search').removeClass("table-sortable__search--active");
+    //         searchReset();
+    //     }
+    // });
 
-    // SORT ICON
-    var sortClickButtons = $(".sort");
-    sortClickButtons.on("click", function() {
-        $(this).children('i.material-icons').text() == "keyboard_arrow_down" ? $(this).children('i.material-icons').text("keyboard_arrow_up") : $(this).children('i.material-icons').text("keyboard_arrow_down");
-    });
+    // // SORT ICON
+    // var sortClickButtons = $(".sort");
+    // sortClickButtons.on("click", function() {
+    //     $(this).children('i.material-icons').text() == "keyboard_arrow_down" ? $(this).children('i.material-icons').text("keyboard_arrow_up") : $(this).children('i.material-icons').text("keyboard_arrow_down");
+    // });
 
-    // FILTER WITH MAP
+    // // FILTER WITH MAP
 
-    $("body").on("click", ".circle", function(){
-        // Remove active map circle selected by dropdown
-        var dropdownSelection = $('#table-dropdown__provinces').children(":selected").attr("id");
-        $('.circle#' + dropdownSelection).attr('class','circle')
-        //Reset dropdown default if map is clicked
-        $('#table-dropdown__provinces').prop('selectedIndex',0);
-        var selectedOption = $(".circle.active").attr("data-province");
-        var allFilters = $(".table-dropdown select");
-        var searchQueries = {};
-        if ($(this).attr('class') == 'circle active') {
-            $('#table-dropdown__provinces').val(selectedOption);
-            searchQueries["filter"] = $(this).attr("id");
-        } else {
-            $(".circle.active").attr("class","circle");
-            $('#table-dropdown__provinces').prop('selectedIndex',0);
-            searchQueries["filter"] = 'all';
-        }
-        filterList();
-    });
+    // $("body").on("click", ".circle", function(){
+    //     // Remove active map circle selected by dropdown
+    //     var dropdownSelection = $('#table-dropdown__provinces').children(":selected").attr("id");
+    //     $('.circle#' + dropdownSelection).attr('class','circle')
+    //     //Reset dropdown default if map is clicked
+    //     $('#table-dropdown__provinces').prop('selectedIndex',0);
+    //     var selectedOption = $(".circle.active").attr("data-province");
+    //     var allFilters = $(".table-dropdown select");
+    //     var searchQueries = {};
+    //     if ($(this).attr('class') == 'circle active') {
+    //         $('#table-dropdown__provinces').val(selectedOption);
+    //         searchQueries["filter"] = $(this).attr("id");
+    //     } else {
+    //         $(".circle.active").attr("class","circle");
+    //         $('#table-dropdown__provinces').prop('selectedIndex',0);
+    //         searchQueries["filter"] = 'all';
+    //     }
+    //     filterList();
+    // });
 
-    // FILTER WITH PROVINCE DROPDOWN
-    $('#table-dropdown__provinces').on('change', function() {
-        $(".circle.active").attr("class","circle");
-        var circleSelect = $(this).children(":selected").attr("id");
-        $('.circle#' + circleSelect).attr({class:'circle active'});
-        companyList.filter();
-    });
+    // // FILTER WITH PROVINCE DROPDOWN
+    // $('#table-dropdown__provinces').on('change', function() {
+    //     $(".circle.active").attr("class","circle");
+    //     var circleSelect = $(this).children(":selected").attr("id");
+    //     $('.circle#' + circleSelect).attr({class:'circle active'});
+    //     companyList.filter();
+    // });
 
-    function filterList(filters) {
-        filters.each(function(idx, selection) {
-            $(selection).each(function(idx, option) {
-                var filterSelection = $(this).attr("data-filter");
-                var option = $(this).children(":selected").attr("id");
-                searchQueries[filterSelection] = option.replace("-mobile", "");
-            });
-        });
-        companyList.filter(function(item) {
-            if ($(".circle.active").length > 0) {
-                // //filter with map selections
-                // searchQueries["filter"] = $(".circle.active").attr("id");
-                // if (item.values()["filter"] !== null
-                // && item.values()["company__provincial-sources"] !== null
-                // && item.values()["company__other-sources"] !== null
-                // && item.values()["company__provinces"] !== null
-                // && item.values()["company__data-sectors"] !== null
-                // && item.values()["company__federal-sources"] !== null
-                // && (item.values()["filter"].indexOf(searchQueries["filter"]) != -1)
-                // && (item.values()["company__provincial-sources"].indexOf(searchQueries["company__provincial-sources"]) != -1)
-                // && (item.values()["company__other-sources"].indexOf(searchQueries["company__data-sources"]) != -1 )
-                // && (item.values()["company__data-sectors"].indexOf(searchQueries["company__data-sectors"]) != -1)
-                // && (item.values()["company__federal-sources"].indexOf(searchQueries["company__federal-sources"]) != -1 )) {
-                //     return true;
-                // } else {
-                //     return false;
-                // }
-            } else {
-                // filter with dropdowns
-                // console.log('item', item.values()["region"], searchQueries["region"], item.values()["region"].toLowerCase().indexOf(searchQueries["region"].toLowerCase()), item);
-                if (item.values()["region"] !== null
-                && item.values()["level"] !== null
-                && item.values()["engagement"] !== null
-                && item.values()["prog"] !== null
-                && (item.values()["region"].toLowerCase().indexOf(searchQueries["region"].toLowerCase()) != -1 )
-                && (item.values()["level"].toLowerCase().indexOf(searchQueries["level"].toLowerCase()) != -1 )
-                && (item.values()["engagement"].toLowerCase().indexOf(searchQueries["engagement"].toLowerCase()) != -1 )
-                && (item.values()["prog"].toLowerCase().indexOf(searchQueries["prog"].toLowerCase())   != -1 )) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-    }
-
-
-    // DROPDOWN FILTERS
-    var allFilters = $(".filters select");
-    var searchQueries = {};
-    allFilters.on("change", function() {
-        if ($(this).hasClass("dropdown-mobile")) {
-            filterList($(".mobile-filters select"));
-        } else {
-            filterList($(".filters select"));
-        }
-    });
-
-    // CLEAR ALL FILTERS
-    $(".clear_filters").on("click", function() {
-        allFilters.each(function(idx,filter) {
-            $('#'+filter.id).prop('selectedIndex',0);
-        });
-        // $(".circle.active").attr("class","circle");
-        companyList.filter();
-        searchReset();
-        companyList.sort('name', { order: "asc" });
-    });
-
-    // tooltips
+    // function filterList(filters) {
+    //     filters.each(function(idx, selection) {
+    //         $(selection).each(function(idx, option) {
+    //             var filterSelection = $(this).attr("data-filter");
+    //             var option = $(this).children(":selected").attr("id");
+    //             searchQueries[filterSelection] = option.replace("-mobile", "");
+    //         });
+    //     });
+    //     companyList.filter(function(item) {
+    //         if ($(".circle.active").length > 0) {
+    //             // //filter with map selections
+    //             // searchQueries["filter"] = $(".circle.active").attr("id");
+    //             // if (item.values()["filter"] !== null
+    //             // && item.values()["company__provincial-sources"] !== null
+    //             // && item.values()["company__other-sources"] !== null
+    //             // && item.values()["company__provinces"] !== null
+    //             // && item.values()["company__data-sectors"] !== null
+    //             // && item.values()["company__federal-sources"] !== null
+    //             // && (item.values()["filter"].indexOf(searchQueries["filter"]) != -1)
+    //             // && (item.values()["company__provincial-sources"].indexOf(searchQueries["company__provincial-sources"]) != -1)
+    //             // && (item.values()["company__other-sources"].indexOf(searchQueries["company__data-sources"]) != -1 )
+    //             // && (item.values()["company__data-sectors"].indexOf(searchQueries["company__data-sectors"]) != -1)
+    //             // && (item.values()["company__federal-sources"].indexOf(searchQueries["company__federal-sources"]) != -1 )) {
+    //             //     return true;
+    //             // } else {
+    //             //     return false;
+    //             // }
+    //         } else {
+    //             // filter with dropdowns
+    //             // console.log('item', item.values()["region"], searchQueries["region"], item.values()["region"].toLowerCase().indexOf(searchQueries["region"].toLowerCase()), item);
+    //             if (item.values()["region"] !== null
+    //             && item.values()["level"] !== null
+    //             && item.values()["engagement"] !== null
+    //             && item.values()["prog"] !== null
+    //             && (item.values()["region"].toLowerCase().indexOf(searchQueries["region"].toLowerCase()) != -1 )
+    //             && (item.values()["level"].toLowerCase().indexOf(searchQueries["level"].toLowerCase()) != -1 )
+    //             && (item.values()["engagement"].toLowerCase().indexOf(searchQueries["engagement"].toLowerCase()) != -1 )
+    //             && (item.values()["prog"].toLowerCase().indexOf(searchQueries["prog"].toLowerCase())   != -1 )) {
+    //                 return true;
+    //             } else {
+    //                 return false;
+    //             }
+    //         }
+    //     });
+    // }
 
 
-    $(".js-tooltip-trigger").on("mouseenter", function(e) {
-        console.log($("#"+$(this).attr('data-tooltip')).length);
-        $("#"+$(this).attr('data-tooltip')).addClass('js-active');
+    // // DROPDOWN FILTERS
+    // var allFilters = $(".filters select");
+    // var searchQueries = {};
+    // allFilters.on("change", function() {
+    //     if ($(this).hasClass("dropdown-mobile")) {
+    //         filterList($(".mobile-filters select"));
+    //     } else {
+    //         filterList($(".filters select"));
+    //     }
+    // });
 
-    });
+    // // CLEAR ALL FILTERS
+    // $(".clear_filters").on("click", function() {
+    //     allFilters.each(function(idx,filter) {
+    //         $('#'+filter.id).prop('selectedIndex',0);
+    //     });
+    //     // $(".circle.active").attr("class","circle");
+    //     companyList.filter();
+    //     searchReset();
+    //     companyList.sort('name', { order: "asc" });
+    // });
 
-    $(".js-tooltip-trigger").on("mousemove", function(e) {
-        var elem = $("#"+$(this).attr('data-tooltip'));
-        elem.css({
-            left:  e.pageX-elem.outerWidth()/2.0 + 'px',
-            top:   e.pageY-elem.outerHeight()-15 + 'px'
-        });
-    });
+    // // tooltips
 
-    $(".js-tooltip-trigger").on("mouseout", function(e) {
-        $("#"+$(this).attr('data-tooltip')).removeClass('js-active');
-        $("#"+$(this).attr('data-tooltip')).css({
-            left: '0px',
-            top:  '0px'
-        });
-    });
+
+    // $(".js-tooltip-trigger").on("mouseenter", function(e) {
+    //     console.log($("#"+$(this).attr('data-tooltip')).length);
+    //     $("#"+$(this).attr('data-tooltip')).addClass('js-active');
+
+    // });
+
+    // $(".js-tooltip-trigger").on("mousemove", function(e) {
+    //     var elem = $("#"+$(this).attr('data-tooltip'));
+    //     elem.css({
+    //         left:  e.pageX-elem.outerWidth()/2.0 + 'px',
+    //         top:   e.pageY-elem.outerHeight()-15 + 'px'
+    //     });
+    // });
+
+    // $(".js-tooltip-trigger").on("mouseout", function(e) {
+    //     $("#"+$(this).attr('data-tooltip')).removeClass('js-active');
+    //     $("#"+$(this).attr('data-tooltip')).css({
+    //         left: '0px',
+    //         top:  '0px'
+    //     });
+    // });
 
     //LOGO FADE
     // var fadeOutLogo = function () {
